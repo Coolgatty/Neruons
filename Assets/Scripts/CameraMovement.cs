217d8771d7a26234e69e278a8597652c5d72c777
Antonio Perez <aperez18@uc.cl>
2020-10-24 22:45:46 -0300

    Procedural Generation

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector2 rotation = new Vector2(0, 0);
+    public float speed = 3;
+    public float moveSpeed = 0.2f;
+    Camera camera;
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+        transform.eulerAngles = (Vector2)rotation * speed;
+
+        transform.position += (transform.forward * Input.GetAxis("Vertical") + transform.up * (Input.GetAxis("Jump") + Input.GetAxis("Down")) + transform.right * Input.GetAxis("Horizontal"))*moveSpeed;
+    }
+}
---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Text;
+using UnityEngine.UI;
+using System.IO;
+using UnityEngine.SceneManagement;
+using System;
+using System.Linq;
+using SimpleFileBrowser;
+
+public class Generator : MonoBehaviour
+{
+    public string filePath;
+    string fileName;
+    public Button fileNameBox;
+    public static byte[] bytes;
+
+    public GameObject neuronPrefab;
+    bool systemCreated = false;
+    List<GameObject> neuronList = new List<GameObject>();
+    int BSI = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == BSI && !systemCreated)
+        {
+            CreateNervousSystem();
+            systemCreated = true;
+        }
+    }
+
+    void UpdateText()
+    {
+        fileNameBox.GetComponentInChildren<Text>().text = fileName;
+    }
+
+    public void OnButtonClicked()
+    {
+        StartCoroutine(ShowLoadDialogCoroutine());
+    }
+
+    public void StartButtonClicked()
+    {
+        if (!string.IsNullOrEmpty(filePath))
+        {
+            SceneManager.LoadSceneAsync(BSI);
+        }
+    }
+
+    private void CreateNervousSystem()
+    {
+        bytes = File.ReadAllBytes(filePath);
+        List<int> byteList = new List<int>(); 
+        List<Vector3> positionList = new List<Vector3>();
+        positionList.Add(Vector3.one);
+        int dim;
+        if (bytes.Length < 1000000)
+        {
+            dim = 1000;
+        }
+        else if (bytes.Length < 1000000000)
+        {
+            dim = 1000000;
+        }
+        else
+        {
+            dim = 1000000000;
+        }
+                
+        for (int i = 0; i < bytes.Length / dim; i++)
+        {
+            byteList.Add(ByteArraySum(SliceByteArray(bytes, dim * i, dim * (i+1))));
+        }
+
+        for (int i = 0; i < byteList.Count; i++)
+        {
+            positionList.Add(new Vector3(UnityEngine.Random.Range(-1.0f, 1.0f), UnityEngine.Random.Range(-1.0f, 1.0f), UnityEngine.Random.Range(-1.0f, 1.0f)) * byteList[i]);
+        }
+        for (int i = 0; i < positionList.Count; i++)
+        {
+           neuronList.Add(Instantiate(neuronPrefab, positionList[i], Quaternion.identity));
+        }
+    }
+
+    byte[] SliceByteArray(byte[] array, int startIndex, int endIndex)
+    {
+        if (endIndex > array.Length)
+        {
+            endIndex = array.Length;
+        }
+        byte[] bytes = new byte[endIndex-startIndex];
+        int j = 0;
+        for (int i = startIndex; i < endIndex; i++)
+        {
+            bytes[j] = array[i];
+            j++;
+        }
+        return bytes;
+    }
+
+    int ByteArraySum(byte[] array)
+    {
+        int total = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            total += array[i];
+        }
+        return total%1000;
+    }
+
+    IEnumerator ShowLoadDialogCoroutine()
+    {
+        yield return FileBrowser.WaitForLoadDialog(false, true, null, "Load File", "Load");
+
+        if (FileBrowser.Success)
+        {
+            for (int i = 0; i < FileBrowser.Result.Length; i++)
+                Debug.Log(FileBrowser.Result[i]);
+            filePath = FileBrowser.Result[0];
+        }
+        fileName = Path.GetFileName(filePath);
+        if (!string.IsNullOrEmpty(fileName))
+        {
+            UpdateText();
+        }
+    }
+}
